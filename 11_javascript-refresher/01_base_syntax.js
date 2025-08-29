e963a0e28ff453b0942c24a7eb5ae731304e4dd7
j4melaF <jamela.fernandez.29@gmail.com>
2025-08-29 22:48:10 +0800

    javascript refresher

---
+++ 11_javascript-refresher/01_base_syntax.js
@@ -0,0 +1,8 @@
+console.log("Hello JavaScript");
+
+// trying the case sensitivity
+let myName = "Jam";
+let myname = "Ela";
+
+console.log(myName);
+console.log(myname);
---
+++ 11_javascript-refresher/02_variables.js
@@ -0,0 +1,22 @@
+let name = "Jamela";
+let age = 20;
+let isStudent = true;
+
+// printing their values here
+console.log(name, age, isStudent);
+
+// checking their typeof
+console.log(typeof name, typeof age, typeof isStudent);
+
+// Doing some Arithmetic
+let a = 20;
+let b = 7;
+
+console.log("Add:", a + b);
+console.log("Subtract:", a - b);
+console.log("Multiply:", a * b);
+console.log("Divide:", a / b);
+
+// Comparing /  Comparison
+console.log("5" == 5);
+console.log("5" === 5);
