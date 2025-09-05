3fdfe909f1252322fbbe4567d3480c9477a1ce17
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-05 11:59:13 +0800

    Add tricky parts activity

--- 11_javascript-refresher/09_tricky_parts.js
+++ 11_javascript-refresher/09_tricky_parts.js
@@ -1,1 +1,40 @@
 
+
+console.log("5" == 5);   // true 
+console.log("5" === 5);  // false 
+
+
+var a;          
+var b = null;   
+
+console.log("a:", a); // undefined
+console.log("b:", b); // null
+
+
+var obj = {
+  regularFunc: function() {
+    console.log("regularFunc this:", this);
+  },
+  arrowFunc: () => {
+    console.log("arrowFunc this:", this);
+  }
+};
+
+obj.regularFunc(); 
+obj.arrowFunc();   
+
+
+var arr1 = [1, 2, 3];
+var arr2 = arr1;   
+
+arr2.push(4);  
+console.log("arr1:", arr1);
+console.log("arr2:", arr2);
+
+
+var arr3 = [1, 2, 3];
+var arr4 = [...arr3];  
+
+arr4.push(4);         
+console.log("arr3:", arr3);
+console.log("arr4:", arr4);
