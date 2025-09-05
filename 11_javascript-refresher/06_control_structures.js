38d009ac324f469e4664481f42f08a2b66407464
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-05 11:58:06 +0800

    Add control structures activity

--- 11_javascript-refresher/06_control_structures.js
+++ 11_javascript-refresher/06_control_structures.js
@@ -1,1 +1,24 @@
 
+let grade = 90;
+
+if (grade >= 90) {
+  console.log("A");
+} else if (grade >= 80) {
+  console.log("B");
+} else if (grade >= 70) {
+  console.log("C");
+} else {
+  console.log("F");
+}
+
+// For loop
+for (let i = 1; i <= 5; i++) {
+  console.log(i);
+}
+
+// While loop
+let count = 0;
+while (count < 3) {
+  console.log("Hello");
+  count++;
+}
