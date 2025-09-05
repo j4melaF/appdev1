3a33b1b5d6a579753438a39cef3f587e11324d44
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-05 11:57:22 +0800

    Add objects activity

--- 11_javascript-refresher/04_objects.js
+++ 11_javascript-refresher/04_objects.js
@@ -1,1 +1,14 @@
 
+let aboutMe = {
+  name: "Jamela",
+  age: 20,
+  course: "BSIT",
+  introduce: function() {
+    console.log(`Hi, my name is ${this.name} and I am ${this.age} years old.`);
+  }
+};
+
+aboutMe.hobby = "Sleeping"; 
+
+aboutMe.introduce();
+console.log("Hobby:", aboutMe.hobby);
