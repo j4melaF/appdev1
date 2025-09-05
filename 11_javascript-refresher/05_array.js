aa6125934612be1a6ca5f1c43a357350c3b5c913
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-05 11:57:42 +0800

    Add arrays activity

--- 11_javascript-refresher/05_array.js
+++ 11_javascript-refresher/05_array.js
@@ -1,1 +1,12 @@
 
+let foods = ["Pizza", "Champorado", "Sushi"];
+
+foods.push("Pasta"); 
+foods.shift();         
+
+for (let food of foods) {
+  console.log(food);
+}
+
+let likes = foods.map(f => "I like " + f);
+console.log(likes);
