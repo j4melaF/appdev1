23e48022cb59ac8beca4cf7bc3ba2c8fda99a148
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-05 11:56:12 +0800

    Add functions activity

--- 11_javascript-refresher/03_function.js
+++ 11_javascript-refresher/03_function.js
@@ -1,1 +1,17 @@
 
+function greet(name) {
+  return "Hello, " + name + "!";
+}
+
+const square = (num) => num * num;
+
+function calculator(a, b) {
+  return {
+    sum: a + b,
+    product: a * b
+  };
+}
+
+console.log(greet("Jamela"));
+console.log(square(4));
+console.log(calculator(3, 5));
