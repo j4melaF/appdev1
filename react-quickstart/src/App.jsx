ee7ece617175aba67e76d001d64c248b4832c971
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-17 15:32:55 +0800

    Done writing markup with JSX

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -1,19 +1,28 @@
-import { useState } from 'react'
+import { useState } from 'tact'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function Nesting() {
-  return (
+    n (
     <button>I am a button</button>
   );
 }
 
 export function Practice() {
-  return (
+n (
     <div>
       <h1>Practicing the nesting.</h1>
       <Nesting />
     </div>
+  );
+}
+
+function MarkUp() {
+  return (
+    <>
+      <h1>Practicing the MarkUp.</h1>
+      <p>This is a paragraph inside <br />the MarkUp component.</p>
+    </>
   );
 }
