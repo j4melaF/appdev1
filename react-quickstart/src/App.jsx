e788c530e0cbd7a264a03d6d0b4ece10834125ca
j4melaF <jamela.fernandez.29@gmail.com>
2025-09-21 10:59:32 +0800

    Done updating the screen / using hooks

--- react-quickstart/src/App.jsx
+++ react-quickstart/src/App.jsx
@@ -77,4 +77,33 @@ function ChangeTextButton() {
   );
 }
 
-export default ChangeTextButton;
+// export default ChangeTextButton;
+
+function MyButton({count, onClick }) {
+  return (
+    <button onClick={onClick}>
+      Clicked {count} times
+    </button>
+  );
+}
+
+
+
+export default function Counter() {
+  const [count, setCount] = useState(0);
+  
+  function handleClick() {
+    setCount(count + 1);
+  }
+
+  return (
+    <div>
+      <h1>Counters that update together</h1>
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+    </div>
+  );
+}
+
+
