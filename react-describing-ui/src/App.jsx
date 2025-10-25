c54525ebac3233988449b2f6e7d16d71e042db8b
j4melaF <jamela.fernandez.29@gmail.com>
2025-10-26 02:18:48 +0800

    Done to use curl braces with JSX in components

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,24 +1,56 @@
-export default function App() {
+// export default function App() {
+//   return (
+//     <div>
+//       <h1>Famous Landmarks</h1>
+//       <img 
+//         src="/img/mayon.jfif" 
+//         alt="Mayon Volcano" 
+//         width="250"
+//       />
+
+//       <br />
+//       <img 
+//         src="/img/chocolatehills.jfif" 
+//         alt="Chocolate Hills" 
+//         width="280"
+//       />
+
+//       <p>
+//         These are some of the <strong>most beautiful landmarks in the Philippines, </strong>
+//         known for their natural beauty and historical value.
+//       </p>
+//     </div>
+//   );
+// }
+
+
+const person = {
+  name: 'Jamela Fernandez',
+  theme: {
+    backgroundColor: 'lightblue',
+    color: 'darkblue',
+    fontFamily: 'Arial, sans-serif',
+    padding: '20px',
+    borderRadius: '10px',
+    width: '50%'
+  }
+}
+
+export default function ToDoList() {
   return (
-    <div>
-      <h1>Famous Landmarks</h1>
+    <div style={person.theme}>
+      <h2>{person.name}'s To-Do List</h2>
       <img 
-        src="/img/mayon.jfif" 
-        alt="Mayon Volcano" 
-        width="250"
-      />
+      className="avatar"
+      src="/img/profile-picture.jpg" 
+      alt="Jamela Fernandez" 
+      width="200" />
 
-      <br />
-      <img 
-        src="/img/chocolatehills.jfif" 
-        alt="Chocolate Hills" 
-        width="280"
-      />
-
-      <p>
-        These are some of the <strong>most beautiful landmarks in the Philippines, </strong>
-        known for their natural beauty and historical value.
-      </p>
+      <ul>
+        <li>Finish React Activity</li>
+        <li>Update Gantt Chart</li>
+        <li>Project Charter</li>
+      </ul>
     </div>
   );
 }
