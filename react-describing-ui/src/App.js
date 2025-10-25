414bb858f101225d01774a9c6f791af648e36850
j4melaF <jamela.fernandez.29@gmail.com>
2025-10-26 04:21:58 +0800

    Done with keepng the components pure

--- react-describing-ui/src/App.js
+++ react-describing-ui/src/App.js
@@ -1,69 +1,109 @@
-import { getImageUrl } from './utils.js'
+// import { getImageUrl } from './utils.js'
 
-export default function Profile() {
-  return (
-    <Card>
-      <Avatar
-        size={100}
-        person={{
-          name: 'Katsuko Saruhashi',
-          imageId: 'YfeOqp2'
-        }}
-      />
-    </Card>
-  );
+// export default function Profile() {
+//   return (
+//     <Card>
+//       <Avatar
+//         size={100}
+//         person={{
+//           name: 'Katsuko Saruhashi',
+//           imageId: 'YfeOqp2'
+//         }}
+//       />
+//     </Card>
+//   );
+// }
+
+// function Avatar({ person, size }) {
+//   return (
+//     <img
+//       className="avatar"
+//       src={getImageUrl(person)}
+//       alt={person.name}
+//       width={size}
+//       height={size}
+//     />
+//   );
+// }
+
+// function Card({ children }) {
+//   return (
+//     <div className="card">
+//       {children}
+//     </div>
+//   );
+// }
+
+// function Item({ name, isPacked }) {
+//   return (
+//     <li className="item">
+//       {name} {isPacked && '✅'}
+//     </li>
+//   );
+// }
+
+// export default function PackingList() {
+//   return (
+//     <section>
+//       <h1>Rimuru's Battle Preparation List</h1>
+//       <ul>
+//         <Item 
+//           isPacked={true} 
+//           name="Slime Core Crystal" 
+//         />
+//         <Item 
+//           isPacked={true} 
+//           name="Demon Lord Cloak" 
+//         />
+//         <Item 
+//           isPacked={false} 
+//           name="Veldora’s Support Scroll" 
+//         />
+//         <Item 
+//           isPacked={false} 
+//           name="Potion Flask" 
+//         />
+//       </ul>
+//     </section>
+//   );
+// }
+
+
+// import { people } from './data.js';
+// import { getImageUrl } from './utils.js';
+
+// export default function List() {
+//   const listItems = people.map(person =>
+//     <li key={person.id}>
+//       <img
+//         src={getImageUrl(person)}
+//         alt={person.name}
+//       />
+//       <p>
+//         <b>{person.name}:</b>
+//         {' ' + person.profession + ' '}
+//         known for {person.accomplishment}
+//       </p>
+//     </li>
+//   );
+//   return (
+//     <article>
+//       <h1>Scientists</h1>
+//       <ul>{listItems}</ul>
+//     </article>
+//   );
+// }
+
+function Cup({ guest }) {
+  return <h2>Tea cup for guest #{guest}</h2>;
 }
 
-function Avatar({ person, size }) {
+export default function TeaSet() {
   return (
-    <img
-      className="avatar"
-      src={getImageUrl(person)}
-      alt={person.name}
-      width={size}
-      height={size}
-    />
-  );
-}
-
-function Card({ children }) {
-  return (
-    <div className="card">
-      {children}
-    </div>
-  );
-}
-
-function Item({ name, isPacked }) {
-  return (
-    <li className="item">
-      {name} {isPacked && '✅'}
-    </li>
-  );
-}
-
-export default function PackingList() {
-  return (
-    <section>
-      <h1>Rimuru's Battle Preparation List</h1>
-      <ul>
-        <Item 
-          isPacked={true} 
-          name="Slime Core Crystal" 
-        />
-        <Item 
-          isPacked={true} 
-          name="Demon Lord Cloak" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Veldora’s Support Scroll" 
-        />
-        <Item 
-          isPacked={false} 
-          name="Potion Flask" 
-        />
-      </ul>
-    </section>
+    <>
+      <Cup guest={1} />
+      <Cup guest={2} />
+      <Cup guest={3} />
+    </>
   );
 }
