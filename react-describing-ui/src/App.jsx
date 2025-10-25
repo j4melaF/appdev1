6472fde3baab1844252602882dc10e4010aa4d27
j4melaF <jamela.fernandez.29@gmail.com>
2025-10-26 01:36:42 +0800

    Done adding markup to JavaScript with JSX

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,21 +1,24 @@
-function Profile() {
+export default function App() {
   return (
-    <img
-      src="/img/profile-picture.jpg"
-      alt="My Photo"
-      
-      
-    />
-  );
-}
+    <div>
+      <h1>Famous Landmarks</h1>
+      <img 
+        src="/img/mayon.jfif" 
+        alt="Mayon Volcano" 
+        width="250"
+      />
 
-export default function Gallery() {
-  return (
-    <section>
-      <h1>My Gallery</h1>
-      <Profile />
-      <Profile />
-      <Profile />
-    </section>
+      <br />
+      <img 
+        src="/img/chocolatehills.jfif" 
+        alt="Chocolate Hills" 
+        width="280"
+      />
+
+      <p>
+        These are some of the <strong>most beautiful landmarks in the Philippines, </strong>
+        known for their natural beauty and historical value.
+      </p>
+    </div>
   );
 }
