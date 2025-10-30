69b289dbc0bafcfc01113bad0242b319e1051a3d
j4melaF <jamela.fernandez.29@gmail.com>
2025-10-30 12:03:29 +0800

    Done with RootComponent

--- react-describing-ui/src/App.jsx
+++ react-describing-ui/src/App.jsx
@@ -1,56 +1,12 @@
-// export default function App() {
-//   return (
-//     <div>
-//       <h1>Famous Landmarks</h1>
-//       <img 
-//         src="/img/mayon.jfif" 
-//         alt="Mayon Volcano" 
-//         width="250"
-//       />
+import {ComponentA} from './ComponentA';
+import {ComponentC} from './ComponentC';
 
-//       <br />
-//       <img 
-//         src="/img/chocolatehills.jfif" 
-//         alt="Chocolate Hills" 
-//         width="280"
-//       />
-
-//       <p>
-//         These are some of the <strong>most beautiful landmarks in the Philippines, </strong>
-//         known for their natural beauty and historical value.
-//       </p>
-//     </div>
-//   );
-// }
-
-
-const person = {
-  name: 'Jamela Fernandez',
-  theme: {
-    backgroundColor: 'lightblue',
-    color: 'darkblue',
-    fontFamily: 'Arial, sans-serif',
-    padding: '20px',
-    borderRadius: '10px',
-    width: '50%'
-  }
-}
-
-export default function ToDoList() {
+export function RootComponent() {
   return (
-    <div style={person.theme}>
-      <h2>{person.name}'s To-Do List</h2>
-      <img 
-      className="avatar"
-      src="/img/profile-picture.jpg" 
-      alt="Jamela Fernandez" 
-      width="200" />
-
-      <ul>
-        <li>Finish React Activity</li>
-        <li>Update Gantt Chart</li>
-        <li>Project Charter</li>
-      </ul>
+    <div>
+      <h1>Root Component</h1>
+      <ComponentA />
+      <ComponentC />
     </div>
   );
 }
