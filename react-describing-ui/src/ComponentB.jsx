f50eebaa0bc894f53f3d6117503df22214ef3fbf
j4melaF <jamela.fernandez.29@gmail.com>
2025-10-30 12:07:04 +0800

    Done with Component B (leaf), child of Component A

---
+++ react-describing-ui/src/ComponentB.jsx
@@ -0,0 +1,10 @@
+
+
+export function ComponentB(leaf) {
+    return (
+        <div>
+            <h3>Component B</h3>
+            <p>This is Component B, a child of Component A.</p>
+        </div>
+    );
+}
