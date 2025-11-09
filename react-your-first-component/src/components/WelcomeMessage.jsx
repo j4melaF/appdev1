c37ba73ab5a71e436ace22a1d91b9333047f3225
j4melaF <jamela.fernandez.29@gmail.com>
2025-11-09 23:06:14 +0800

    Done with defining WelcomeMessage component

---
+++ react-your-first-component/src/components/WelcomeMessage.jsx
@@ -0,0 +1,10 @@
+function WelcomeMessage({ name }) {
+    return(
+        <div>
+            <h1>Hello, {name}!</h1>
+            <p>Welcome to my first component.</p>
+        </div>
+    )
+}
+
+export default WelcomeMessage;
