17adb25396ff76aa0068d3ea3cc2dbd8d5d7812b
j4melaF <jamela.fernandez.29@gmail.com>
2025-11-09 23:33:25 +0800

    Done with exporting and importing a component

---
+++ react-importing-exporting-components/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import RootComponent, {Header, Content, Footer} from "./RootComponent.jsx";
+
+const App = () => {
+  return(
+    <div>
+      {/* using the default component */}
+      <RootComponent />
+    </div>
+  )
+}
+
+export default App;
